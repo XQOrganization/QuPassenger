e21576ad3d95106bbd2df291a439ace94d93f5c7
朱青 <614128859@qq.com>
2017-09-26 10:18:19 +0800

    upload main

---
+++ QuPassenger/QuPassenger/VCs/Main/View/MainLeftCateCell.h
@@ -0,0 +1,16 @@
+//
+//  MainLeftCateCell.h
+//  QuPassenger
+//
+//  Created by 朱青 on 2017/9/26.
+//  Copyright © 2017年 com.Qyueche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainLeftCateCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *cateLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *cateImageView;
+
+@end
---
+++ QuPassenger/QuPassenger/VCs/Main/View/MainLeftCateCell.m
@@ -0,0 +1,24 @@
+//
+//  MainLeftCateCell.m
+//  QuPassenger
+//
+//  Created by 朱青 on 2017/9/26.
+//  Copyright © 2017年 com.Qyueche. All rights reserved.
+//
+
+#import "MainLeftCateCell.h"
+
+@implementation MainLeftCateCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
