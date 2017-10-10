45cea4df70451d5adaf12fa09ec33a2979f4df51
陈目松 <296153243@qq.com>
2017-10-10 10:48:08 +0800

    dddddd

---
+++ QuPassenger/QuPassenger/VCs/Main/Controller/MessageVC.h
@@ -0,0 +1,13 @@
+//
+//  MessageVC.h
+//  QuPassenger
+//
+//  Created by Chenmusong on 2017/10/10.
+//  Copyright © 2017年 com.Qyueche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageVC : UIViewController
+
+@end
---
+++ QuPassenger/QuPassenger/VCs/Main/Controller/MessageVC.m
@@ -0,0 +1,23 @@
+//
+//  MessageVC.m
+//  QuPassenger
+//
+//  Created by Chenmusong on 2017/10/10.
+//  Copyright © 2017年 com.Qyueche. All rights reserved.
+//
+
+#import "MessageVC.h"
+
+@interface MessageVC ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *messagetableView;
+
+@end
+
+@implementation MessageVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
